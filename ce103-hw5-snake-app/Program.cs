e7c147c09c576b5f5678c03b2eaccaab7944b339
mehmet alpergun <mehmet_alpergun20@erdogan.edu.tr>
2022-01-07 16:24:48 +0300

    added

---
+++ ce103-hw5-snake-app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ce103_hw5_mehmet_alpergun;
+
+namespace ce103_hw5_snake_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            appclass snake = new appclass();
+            snake.main();
+        }
+    }
+}
